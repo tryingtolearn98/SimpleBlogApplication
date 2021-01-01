c6cd766f7938414f9e91e4393f8c67aefd0e05a0
Robi <goddessjamie@yahoo.com>
2021-01-01 12:00:19 +0200

    User can create a account
    Now, the user can create a new account from the public interface.

--- SimpleBlogProject/urls.py
+++ SimpleBlogProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from users import views as users_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_view.register, name='register'),
     path('', include('blog.urls'))
 ]
