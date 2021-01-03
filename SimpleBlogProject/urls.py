cf23406262586e09e409af3a4f93c5774f810f44
Robi <goddessjamie@yahoo.com>
2021-01-03 21:23:51 +0200

    Posts functionalities and Login functionality
    - User can Add/Remove/Update Posts.
    - User can Login into the Application.

--- SimpleBlogProject/urls.py
+++ SimpleBlogProject/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as users_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', users_view.register, name='register'),
+    path('profile/', users_view.user_profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
     path('', include('blog.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -10,7 +11,29 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def user_profile(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+
+        if user_form.is_valid() and profile_update_form.is_valid():
+            user_form.save()
+            profile_update_form.save()
+            messages.success(request, f'Account updated!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.userprofile)
+
+    context = {
+        'user_form': user_form,
+        'profile_update_form': profile_update_form
+    }
+    return render(request, 'users/profile.html', context)
